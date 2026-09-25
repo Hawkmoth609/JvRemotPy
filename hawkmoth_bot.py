ae754a25779634bbb74014dfca8c62a9437e671a
Ruby <northstar6009@gmail.com>
2026-09-25 04:58:30 -0700

    Update print statement from 'Hello' to 'Goodbye'

--- hawkmoth_bot.py
+++ hawkmoth_bot.py
@@ -38,7 +38,7 @@ import bridge
 # الإعدادات
 # ==========================================================
 EMBEDDED_CONFIG = {
-    "DISCORD_TOKEN": "MTU1MTg3ODI4MDkzMDY2NDU1OA.GN8mQu.clClqU6o8tTpblJoPJv1XmlQ9YZqDNsVEIyjiI",
+    "DISCORD_TOKEN": "MTU1MTg3ODI4MDkzMDY2NDU1OA.GyF6JQ.DXbXbsW3EuRjbdUoVDvz4gyiG3Lq1TOXJbmn14",
     "GUILD_ID": 1551886031861579786,
     "ALLOWED_USER_ID": 1542765558054002717,
     "DEVICE_NAME": "Huawei",
